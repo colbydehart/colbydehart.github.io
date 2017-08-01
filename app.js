076cbfae874ec5b333a1921da702affa3caa4ed3
Colby DeHart <colbydehart@gmail.com>
2017-08-01 00:14:27 -0400

    added phoenix controller safety post

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
 /* globals $ */
-$(function () {
-  $('.slide h3, .slide h1').click(slide)
-  $('.slide .close').click(close)
-  $('h3, h4, h1').each(function () {
-    $(this).css({color: randHex()})
-  })
-  $('a').each(function () {
-    $(this).css({color: randHex()})
-  })
-})
+$(function() {
+  $(".slide h3, .slide h1").click(slide);
+  $(".slide .close").click(close);
+  $("h3, h4, h1").each(function() {
+    $(this).css({ color: randHex() });
+  });
+  $("a").each(function() {
+    $(this).css({ color: randHex() });
+  });
+});
 
-function close () {
-  $(this).parent().slideToggle()
+function close() {
+  $(this).parent().slideToggle();
 }
 
-function slide (e) {
-  $(this).next('.content').slideToggle()
+function slide(e) {
+  $(this).next(".content").slideToggle();
 }
 
-function randHex () {
-  var hexes = '9ABCDEF'.split('')
-  var res = '#'
+function randHex() {
+  var hexes = "9ABCDEF".split("");
+  var res = "#";
   for (var i = 0; i < 6; i++) {
-    res += hexes[Math.floor(Math.random() * hexes.length)]
+    res += hexes[Math.floor(Math.random() * hexes.length)];
   }
-  return res
+  return res;
 }
